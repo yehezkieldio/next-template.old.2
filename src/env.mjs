2008cb829cf18f42a32f271e1b6ce59581c904f9
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-04-30 01:26:07 +0800

    feat: add stuff 6

--- src/env.mjs
+++ src/env.mjs
@@ -9,6 +9,9 @@ export const env = createEnv({
     server: {
         DATABASE_URL: z.string().url(),
         NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+        AUTH_SECRET: z.string(),
+        AUTH_DISCORD_ID: z.string(),
+        AUTH_DISCORD_SECRET: z.string(),
     },
 
     /**
@@ -27,6 +30,9 @@ export const env = createEnv({
     runtimeEnv: {
         DATABASE_URL: process.env.DATABASE_URL,
         NODE_ENV: process.env.NODE_ENV,
+        AUTH_SECRET: process.env.AUTH_SECRET,
+        AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
+        AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     },
     /**
      * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
